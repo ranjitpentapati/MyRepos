da459b6f1440f6025756b8adc0be4aa7325174b3
ranjitpentapati <rk3054890@gmail.com>
2015-03-06 01:23:24 +0530

    new change
    new change

--- master/SpringBoot/src/main/java/hello/Application.java
+++ master/SpringBoot/src/main/java/hello/Application.java
@@ -1,6 +1,13 @@
 package hello;
 
 import java.util.Arrays;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
